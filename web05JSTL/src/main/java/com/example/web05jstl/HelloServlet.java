72befb24e47e37f89004418f5ecab250afe27d2d
Lee Sumin <bluky0516@gmail.com>
2024-08-01 16:52:52 +0900

    create testVO, index2

--- web05JSTL/src/main/java/com/example/web05jstl/HelloServlet.java
+++ web05JSTL/src/main/java/com/example/web05jstl/HelloServlet.java
@@ -1,10 +1,15 @@
 package com.example.web05jstl;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet({"/hello-servlet","/index.do","/index2.do"})
 public class HelloServlet extends HttpServlet {
     private String message;
 
@@ -12,14 +17,38 @@ public class HelloServlet extends HttpServlet {
         message = "Hello World!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        String sPath = request.getServletPath();
+        if(sPath.equals("/hello-servlet")){
+            response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+            // Hello
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<h1>" + message + "</h1>");
+            out.println("</body></html>");
+        }else if(sPath.equals("/index.do")){
+
+            List<TestVO> list = new ArrayList<>();
+
+
+            for (int i = 0; i < 5; i++) {
+                TestVO vo = new TestVO();
+                vo.setName("kim"+i);
+                vo.setAge(10+i);
+                list.add(vo);
+            }
+
+            request.setAttribute("list",list);
+
+            RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+            rd.forward(request,response);
+        }else if(sPath.equals("/index2.do")){
+
+            RequestDispatcher rd = request.getRequestDispatcher("index2.jsp");
+            rd.forward(request,response);
+        }
     }
 
     public void destroy() {

cf4971ca3ef84ac98e862c15007afafaeb2cd3aa
Lee Sumin <bluky0516@gmail.com>
2024-08-02 14:27:12 +0900

    feat: linked to jdbc

--- web06jdbc/src/main/java/com/example/web06jdbc/HelloServlet.java
+++ web06jdbc/src/main/java/com/example/web06jdbc/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.example.web06jdbc;
 
 import java.io.*;
+import java.sql.*;
 
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -8,6 +9,20 @@ import jakarta.servlet.annotation.*;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+
+    // jdbc 라이브러리 세팅(경로설정)
+    // C:\oraclexe\app\oracle\product\11.2.0\server\jdbc\lib : ojdbc6.jar 복사
+    // 내 프로젝트 >> lib 폴더(없다면 생성)에 >> 복붙
+    // 상단 메뉴> File > Project Structure > Libraries > + : java 선택 > 복붙경로 > ojdbc6.jar 선택
+
+    // DB 전역변수 설정
+    private static final String DRIVER_NAME = "oracle.jdbc.OracleDriver";
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String USER = "JAVA";
+    private static final String PASSWORD = "hi123456";
+    private Connection conn;//커넥션객체
+    private PreparedStatement pstmt;//쿼리(sql문-CRUD)실행객체
+    private ResultSet rs;//select문 리턴 객체
 
     public void init() {
         message = "Hello World!";
@@ -22,10 +37,20 @@ public class HelloServlet extends HttpServlet {
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
 
+        // 오라클 드라이버 클래스 연동
         try {
-            Class.forName("oracle.jdbc.OracleDriver");
+            Class.forName(DRIVER_NAME);
             System.out.println("Driver successed...");
         } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // 커넥션
+        try {
+            conn = DriverManager.getConnection(URL,USER,PASSWORD);
+            System.out.println("conn successed...");
+
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
